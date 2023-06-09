f3f52fe437c06717416c6323fb998030cc412e26
Viverino1 <Vivek.maddineni@gmail.com>
2023-06-09 18:02:32 -0500

    e

--- src/pages/cards/cards.tsx
+++ src/pages/cards/cards.tsx
@@ -6,7 +6,7 @@ export default function Cards(){
     
     return(
         <div className="relative w-full h-screen p-4 overflow-scroll">
-            <div className="z-30 fixed left-25 right-4 top-4 h-16 rounded-lg bg-primary shadow-primary shadow-lg hover:shadow-2xl hover:shadow-primary transition-all duration-300"></div>
+            <div className="z-30 fixed left-25 right-4 top-4 h-16 rounded-lg bg-primary shadow-primary shadow-md hover:shadow-sm hover:shadow-primary transition-all duration-300"></div>
             <div className="h-20 w-full"/>
             <div className="lg:grid-cols-3 md:grid-cols-2 inline-grid w-full gap-4">
                 {evidences.map((card, index) => (
--- src/pages/expanded/EvidenceCardExpanded.tsx
+++ src/pages/expanded/EvidenceCardExpanded.tsx
@@ -3,6 +3,7 @@ import { contsub } from "../../utils/helpers";
 import { Evidence, User } from "../../utils/types";
 import { getUser } from "../../utils/firebase/firestore";
 import Loading from "../loading/Loading";
+import { Pen, Share, Trash3 } from "react-bootstrap-icons";
 
 export default function EvidenceCardExpanded(props: {data: Evidence}){
     const [loading, setLoading] = useState(true);
@@ -27,8 +28,9 @@ export default function EvidenceCardExpanded(props: {data: Evidence}){
             <div className="text-2xl mt-4">Reasoning</div>
             <div className="text-lg">{props.data.reasoning}</div>
 
-            <div className="w-fit h-fit p-4 mt-4 rounded-lg bg-secondary">
+            <div className="flex flex-col space-y-2 w-fit h-fit p-4 mt-4 rounded-lg bg-secondary">
                 <div className="text-2xl">Creator</div>
+                <div className="w-full h-px bg-accent"/>
                 <div className="flex space-x-4 items-center">
                     <img src={owner.photoURL} className="w-16 h-16 rounded-full"/>
                     <div>
@@ -36,6 +38,21 @@ export default function EvidenceCardExpanded(props: {data: Evidence}){
                         <div>{owner.email}</div>
                         <div>Speaker {owner.speaker}</div>
                     </div>
+                </div>
+            </div>
+            <div className="flex flex-col space-y-2 w-fit h-fit p-4 mt-4 rounded-lg bg-secondary">
+                <div className="text-2xl">Options</div>
+                <div className="w-full h-px bg-accent"/>
+                <div className="flex space-x-4 text-secondary">
+                    <button className="flex justify-center items-center w-16 h-16 bg-primary rounded-full">
+                        <Pen size={30}/>
+                    </button>
+                    <button className="flex justify-center items-center w-16 h-16 bg-primary rounded-full">
+                        <Trash3 size={30}/>
+                    </button>
+                    <button className="flex justify-center items-center w-16 h-16 bg-primary rounded-full">
+                        <Share size={30}/>
+                    </button>
                 </div>
             </div>
         </div>
